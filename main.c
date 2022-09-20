0c4061befb9ed1501d39754360803da9e1801b68
usoobin <112929005+usoobin@users.noreply.github.com>
2022-09-20 17:17:08 +0900

    20220920

--- main.c
+++ main.c
@@ -3,18 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) 
+int main(int argc, char *argv[]) 
 {
-	int input_int;
-	float input_float;
+	char text;
 	
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
+	printf("enter a character : ");
+	scanf("%c", &text);
 	
-	printf("enter a float : ");
-	scanf("%f", &input_float);
+	printf("the next character of %c (%d) is %c (%d)\n",text, text, text+1 ,text+1);
 	
-	printf("integer : %d, float : %f\n", input_int, input_float);
-
 	return 0;
 }
